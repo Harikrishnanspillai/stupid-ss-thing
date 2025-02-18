643bdaa36bfbba87424afd914587ff2a6c4b4939
Harikrishnanspillai <harikrishnansugesh@gmail.com>
2025-02-18 09:44:41 +0530

    FIRST COMMIT

---
+++ README.md
@@ -0,0 +1,11 @@
+Dependencies
+====================================================
+
+tesseract
+pytesseract
+ollama
+pillow
+
+that's bout it
+
+P.S. I'm pretty sure this will only work in windows, haven't tried on linux yet tho
---
+++ aiocr.py
@@ -0,0 +1,42 @@
+import os
+from glob import glob
+from ollama import chat
+from PIL import Image
+from pytesseract import image_to_string as ist
+
+def bot(inp):
+    stream = chat(
+        model='dolphin-mistral:latest',
+        messages=[{'role': 'user', 'content': inp}],
+        stream=True,
+    )
+
+    for chunk in stream:
+      print(chunk['message']['content'], end='', flush=True)
+    print('\n\n')
+
+def latestimg():
+      dir="C://Users/harik/OneDrive/Pictures/Screenshots 1"
+      files=glob(os.path.join(dir, "*.png"))
+      if not files:
+         print('No files')
+         return None
+      latest=max(files, key=os.path.getctime)
+      return latest
+
+latest=latestimg()
+os.system('snippingtool.exe')
+img=Image.open(latest)
+s=ist(img)
+print(f"Orginal:\n{s}")
+inp=f"Hey, i used a ocr to get this text from and image, pls go thru it and check and correct any errors(don't remove stuff unless absolutley unnecessary)\n\n{s}"
+bot(inp)
+os.system(f'del {latest}')
+
+while True:
+  msg=input("Enter msg: ")
+  bot(msg)
+  if msg.lower()=="/bye":
+    break
+input('Press enter to exit the program')
+
